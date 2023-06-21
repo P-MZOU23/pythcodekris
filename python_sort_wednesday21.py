84f41a779725c7b344dde0c0c4d408aaffa90152
P-MZOU23 <136847563+P-MZOU23@users.noreply.github.com>
2023-06-21 14:43:39 -0500

    Update python_sort_wednesday21.py

--- python_sort_wednesday21.py
+++ python_sort_wednesday21.py
@@ -5,10 +5,10 @@ def sequential_search(number_list, target_value):
        #determine if number at current index = target_value index
        #if so, return index and end loop
        if target_value == number_list[index]:
-           return index and print("NOTE TO SELF: This index is the target_value minus one.")
+           return index
        
     return -1
-    print("NOTE TO SELF: Remember that -1 is not in the list. This is the default if the target value was not in the list.")
+
 
 def main():
     #use 101 as top limit
@@ -22,3 +22,5 @@ def main():
 
 
 main()
+
+#add explanation notes that printwith results
